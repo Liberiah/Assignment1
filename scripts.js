61a9a67f601756453c10fccc68b5d77bf2c9459c
Liberiah <elizabeth.young@tri-c.edu>
2017-02-07 20:34:53 -0500

    cont'd phase 1 upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+
+function myFunction()
+{
+	//document.getElementById("demo").innerHTML = "call 216-555-5555 for more info on this tour";
+	alert("Call 216-555-5555 for more information on this tour.");
+}
+
